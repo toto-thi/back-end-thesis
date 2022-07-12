922743fcbe1cdbbcefbe5f033303d8f19b3f5fb3
Toto <66200832+toto-thi@users.noreply.github.com>
2022-07-13 00:35:15 +0700

    Update setToken.js
    - change expires time to 7days

--- src/helpers/setToken.js
+++ src/helpers/setToken.js
@@ -2,6 +2,6 @@ import { sign } from "jsonwebtoken";
 
 export const issueToken = (user) => {
   return sign({ userId: user.id }, process.env.ACCESS_TOKEN, {
-    expiresIn: "1h",
+    expiresIn: "7d",
   });
 };
